b1bdc4b3139dfa8b7f3ac9d41a051030bc13a5ef
Libardo Cuello <libardoii@hotmail.com>
2021-04-25 11:06:31 -0500

    correction type username to String, add user type to responde task

--- graphql_app/resolvers/task_resolver.py
+++ graphql_app/resolvers/task_resolver.py
@@ -7,7 +7,12 @@ def list_tasks(*_):
             "title": task.title,
             "description": task.description,
             "start_date": task.start_date,
-            "end_date": task.end_date
+            "end_date": task.end_date,
+            "user": {
+                "username": task.user.username,
+                "first_name": task.user.first_name,
+                "last_name": task.user.last_name
+            }
         }
         for task in Task.objects.all()
     ]
